45623ea085266d217527c94ce607e9eb34943ab7
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-13 10:29:28 -0800

    add html file for form

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,18 @@ var triangleMaker = function(side1, side2, side3) {
     return "scalene";
   };
 };
+
+$(document).ready(function() {
+  $('form#triangle-maker').submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($('input#side1').val());
+    var side2 = parseInt($('input#side2').val());
+    var side3 = parseInt($('input#side3').val()):
+    var result = triangleCalculator(side1, side2, side3);
+
+    if (result === false) {
+      result = 'not at all a'
+      $('.triangle').text('a(n)' + result);
+    }
+  });
+});
