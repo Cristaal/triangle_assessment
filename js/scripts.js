7af32a8b0c1a98c36f2ce80356161f8107a3dced
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-16 20:02:57 -0800

    fix bug in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var triangleMaker = function(side1, side2, side3) {
   if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
     return false;
-  } else if (side1 + side2 < side3) {
+  } else if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
     return false;
   } else if (side1 === side2 && side1 === side3) {
     return "equilateral";
