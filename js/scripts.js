5b14f960ee700423511f3211e3e974747719eeb4
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-19 21:42:38 -0800

    add spec for returning false for empty input field

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var triangleMaker = function(side1, side2, side3) {
-  if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+  if ( isNaN(side1) || isNaN(side2) || isNaN(side3) ) {
+    return "Invalid";
+  } else if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
     return false;
   } else if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
     return false;
@@ -20,8 +22,11 @@ $(document).ready(function() {
     var side3 = parseInt($('input#side3').val());
     var result = triangleMaker(side1, side2, side3);
 
-    if (result === false) {
-      result = 'This is not at all a triangle'
+    if (result === "Invalid") {
+      alert('Please enter a set of numbers');
+      $(".triangle").text("Please enter a set of numbers");
+    } else if (result === false) {
+      alert('This is not at all a triangle');
       $(".triangle").text(result);
     } else {
       $(".triangle").text(result);
